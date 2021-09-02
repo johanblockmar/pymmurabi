6ca363cba4007b8709b310a1a9d2a3cffead0b55
Johan Blockmar <johan.blockmar@flexworks.com>
2021-09-02 21:41:11 +0200

    Initial commit of main game file.

---
+++ pymmurabi.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    print("                HAMURABI")
+    print("CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY")
+    print("\n\n\n")
+    print("TRY YOUR HAND AT GOVERNING ANCIENT SUMERIA")
+    print("SUCCESSFULLY FOR A TEN-YEAR TERM OF OFFICE.\n")
+    
+
+if __name__ == "__main__":
+    main()
